3e8a6df14701172f2b25517065b6c956fc1c1007
seany777 <iammuyaholover@gmail.com>
2026-09-11 14:17:48 +0900

    last2

--- 04_collection/ex4.py
+++ 04_collection/ex4.py
@@ -131,7 +131,17 @@ stocks = (
 )
 
 # 총 재고 금액 출력
-total = sum(price * pro for _, price, pro in stocks)
-total = sum([price * pro for _, price, pro in stocks])
+total = sum(price * qty for _, price, qty in stocks)
+total = sum([price * qty for _, price, qty in stocks])
 # ✅ 총액: 21,000원
+print(f"총액: {total:,}원")
+
+
+stocks = (
+    ("사과", "바나나", "체리"),
+    (1000, 2000, 5000),
+    (5,3,2),
+)
+
+total=sum(price * qty for _,price,qty in zip(*stocks))
 print(f"총액: {total:,}원")
